caffd02ba383ba8ec7318edc970c05d4c8b59e8b
Nathan Totten <ntotten@salesforce.com>
2018-03-14 14:42:14 -0400

    Added walkscore project

---
+++ walk-score/main/default/aura/WalkScore/WalkScoreController.js
@@ -0,0 +1,7 @@
+({
+  doInit: function(component, event, helper) {
+    //var postalCode = event.getParam("postalCode");
+    event.setParam("v.score", "64%");
+    event.setParam("v.status", "green");
+  }
+});
